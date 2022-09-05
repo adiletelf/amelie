7746311c4d755e048fec6adc9420e5c10461d32e
adiletelf <74559101+adiletelf@users.noreply.github.com>
2022-09-05 20:43:37 +0600

    Create controllers

---
+++ src/Amelie.Backend/Controllers/AuthorController.cs
@@ -0,0 +1,100 @@
+﻿using Amelie.Backend.Models;
+using Amelie.Backend.Models.Dto;
+using Amelie.Backend.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Amelie.Backend.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthorController : ControllerBase
+{
+    private readonly IAuthorRepository _repository;
+
+    public AuthorController(IAuthorRepository repository)
+    {
+        _repository = repository;
+    }
+
+    [HttpGet]
+    [ProducesResponseType(typeof(IEnumerable<Author>), 200)]
+    public IActionResult GetAll()
+    {
+        return Ok(_repository.GetAll());
+    }
+
+    [HttpGet("{id}")]
+    [ProducesResponseType(typeof(Author), 200)]
+    [ProducesResponseType(404)]
+    public async Task<IActionResult> GetById(int id)
+    {
+        var author = await _repository.GetByIdAsync(id);
+        if (author is null)
+        {
+            return NotFound($"The record with id: {id} was not found.");
+        }
+
+        return Ok(author);
+    }
+
+    [HttpPost]
+    [ProducesResponseType(typeof(Author), 201)]
+    public async Task<IActionResult> Create(CreateUpdateAuthor dto)
+    {
+        var created = await _repository.Create(dto.FromDto());
+        return CreatedAtAction(nameof(GetById), new { id = created.AuthorId }, created);
+    }
+
+    [HttpPut("{id}")]
+    [ProducesResponseType(typeof(Author), 200)]
+    [ProducesResponseType(404)]
+    public async Task<IActionResult> Update(CreateUpdateAuthor dto, int id)
+    {
+        var author = dto.FromDto();
+        author.AuthorId = id;
+
+        var updated = await _repository.Update(author);
+        if (updated is null)
+        {
+            return NotFound($"The record with id: {id} was not found.");
+        }
+
+        return Ok(updated);
+    }
+
+    [HttpDelete("{id}")]
+    [ProducesResponseType(204)]
+    public IActionResult Remove(int id)
+    {
+        var author = new Author { AuthorId = id };
+        _repository.Remove(author);
+
+        return NoContent();
+    }
+
+    [HttpPost("{id}/add-books")]
+    public async Task<IActionResult> AddBooks(int id, IEnumerable<int> bookIds)
+    {
+        var exists = await _repository.Exists(id);
+        if (!exists)
+        {
+            return NotFound($"The author with authorId: {id} was not found.");
+        }
+
+        await _repository.AddBooks(id, bookIds);
+        return NoContent();
+    }
+
+    [HttpPost("{id}/remove-books")]
+    public async Task<IActionResult> RemoveBooks(int id, IEnumerable<int> bookIds)
+    {
+        var exists = await _repository.Exists(id);
+        if (!exists)
+        {
+            return NotFound($"The author with authorId: {id} was not found.");
+        }
+
+        await _repository.RemoveBooks(id, bookIds);
+        return NoContent();
+    }
+}
---
+++ src/Amelie.Backend/Controllers/BookController.cs
@@ -0,0 +1,101 @@
+﻿using Amelie.Backend.Models;
+using Amelie.Backend.Models.Dto;
+using Amelie.Backend.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Amelie.Backend.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class BookController : ControllerBase
+{
+    private readonly IBookRepository _repository;
+
+    public BookController(IBookRepository repository)
+    {
+        _repository = repository;
+    }
+
+    [HttpGet]
+    [ProducesResponseType(typeof(IEnumerable<Book>), 200)]
+    public IActionResult GetAll()
+    {
+        return Ok(_repository.GetAll());
+    }
+
+    [HttpGet("{id}")]
+    [ProducesResponseType(typeof(Book), 200)]
+    [ProducesResponseType(404)]
+    public async Task<IActionResult> GetByIdAsync(int id)
+    {
+        var book = await _repository.GetByIdAsync(id);
+        if (book is null)
+        {
+            return NotFound($"The record with id: {id} was not found.");
+        }
+
+        return Ok(book);
+    }
+
+    [HttpPost]
+    [ProducesResponseType(typeof(Book), 201)]
+    public async Task<IActionResult> Create(CreateUpdateBook dto)
+    {
+        var created = await _repository.Create(dto.FromDto());
+        return CreatedAtAction(nameof(GetByIdAsync), new { id = created.BookId }, created);
+    }
+
+    [HttpPut("{id}")]
+    [ProducesResponseType(typeof(Book), 200)]
+    [ProducesResponseType(404)]
+    public async Task<IActionResult> Update(CreateUpdateBook dto, int id)
+    {
+        var book = dto.FromDto();
+        book.BookId = id;
+
+        var updated = await _repository.Update(book);
+        if (updated is null)
+        {
+            return NotFound($"The record with id: {id} was not found.");
+        }
+
+        return Ok(updated);
+    }
+
+    [HttpDelete("{id}")]
+    [ProducesResponseType(204)]
+    public async Task<IActionResult> Delete(int id)
+    {
+        var book = new Book { BookId = id };
+        await _repository.Remove(book);
+
+        return NoContent();
+    }
+
+
+    [HttpPost("{id}/add-authors")]
+    public async Task<IActionResult> AddAuthors(int id, IEnumerable<int> authorIds)
+    {
+        var exists = await _repository.Exists(id);
+        if (!exists)
+        {
+            return NotFound($"The book with bookId: {id} was not found.");
+        }
+
+        await _repository.AddAuthorsAsync(id, authorIds);
+        return NoContent();
+    }
+
+    [HttpPost("{id}/remove-authors")]
+    public async Task<IActionResult> RemoveAuthors(int id, IEnumerable<int> authorIds)
+    {
+        var exists = await _repository.Exists(id);
+        if (!exists)
+        {
+            return NotFound($"The book with bookId: {id} was not found.");
+        }
+
+        await _repository.RemoveAuthorsAsync(id, authorIds);
+        return NoContent();
+    }
+}
